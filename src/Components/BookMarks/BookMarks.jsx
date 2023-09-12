6db51357a6e751daf836299016cd2a84a693577f
rifat3790 <mdrifayethossen@gmail.com>
2023-09-11 22:36:41 -0700

    clicked bookmarks text added

--- src/Components/BookMarks/BookMarks.jsx
+++ src/Components/BookMarks/BookMarks.jsx
@@ -1,12 +1,21 @@
 
-
+import PropTypes from 'prop-types';
+import Bookmark from '../Bookmark/Bookmark';
 const BookMarks = ({bookmarks}) => {
-    
     return (
-        <div className="md:w-1/3">
-            <h2>Bookmarked Blogs: {bookmarks.length} </h2>
+        
+        <div className="md:w-1/3 bg-gray-300 ml-4 pt-8 mt-10">
+            <h2 className='text-3xl text-center '>Bookmarked Blogs: {bookmarks.length} </h2>
+            {
+                bookmarks.map(bookmark => <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark> )
+            }
         </div>
+        
     );
 };
+
+BookMarks.propTypes = {
+    bookmarks: PropTypes.array
+}
 
 export default BookMarks;
